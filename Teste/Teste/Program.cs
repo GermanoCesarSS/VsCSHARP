a617318c6cd47a28e2c62ecddaa6a90ffbfbda11
GermanoCesarSS <germanosantos.cesar@gmail.com>
2022-11-24 23:19:59 -0300

    Adicionar arquivos de projeto.

---
+++ Teste/Teste/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization; //CultureInfo.InvariantCulture , = .
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teste
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Germano");
+            Console.ReadLine();
+
+        }
+    }
+}
